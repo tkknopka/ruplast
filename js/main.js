8fd1cdc705f0d86d9f79976c2e26a80fcdb8095d
tkknopka <50095391+tkknopka@users.noreply.github.com>
2021-06-16 11:26:18 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Прелоадер
+$(window).on('load', function () {
+    var $preloader = $('#p_prldr'),
+        $svg_anm   = $preloader.find('.svg_anm');
+    $svg_anm.fadeOut();
+    $preloader.delay(500).fadeOut('slow');
+});
+// Плавный скролл по ссылкам
+$("a").click(function () {
+    var elementClick = $(this).attr("href");
+    var destination = $(elementClick).offset().top;
+    $('html, body').animate({ scrollTop: destination }, 600);
+    return false;
+});
